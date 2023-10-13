7e8e2d1ee818d7d5e672d2e70a1207d6ed4f3e48
Sosa Max Planck <s.sosa@live.fr>
2023-10-13 16:17:04 +0200

    Variance update

--- 4. Simulation.R
+++ 4. Simulation.R
@@ -1,6 +1,7 @@
 ###############################
 ##  Simulations parameters ####
 ###############################
+###############################
 simulations <- function(
     Reps = 100,
     N_id =  seq(30, 100, by = 20),
@@ -19,7 +20,8 @@ simulations <- function(
     exposure_sigma = 2.9,
     exposure_baseline = 40,
     strand = T, # use STRAN model
-    ncores = 1 # number of cores
+    ncores = 1, # number of cores
+    run_blocks_model = T
 ){
   require(parallel)
   require(foreach)
@@ -44,16 +46,13 @@ simulations <- function(
     library(ANTs)
     library(STRAND)
     source("3. Data simulation.R")
-
+    RESULTS = NULL
     get_res = function(x) {
       y = c(coef(x)[2],confint(x, level = 0.9)[2,])
       return(y)
     }
 
-    #' "Bayesian P value". 1)The integral over the posterior from negative infinity to zero,
-    #'                    2) the integral over the posterior from zero to infinity. T
-    #' This is easy to approximate from the samples:
-
+    #' "Bayesian P value".------------
     P_se = function(x){
       M_x = mean(x)
 
@@ -68,7 +67,26 @@ simulations <- function(
       return(P_x/N_x)
     }
 
-    RESULTS = NULL
+    # Variation --------------
+    range_sr_sigma = seq(from = 0.5, to =3, by = 0.2)
+    range_sr_rho = seq(from = 0.4, to = 0.9, by = 0.1)
+
+    range_dr_sigma = seq(from = 0.5, to =3, by = 0.2)
+    range_dr_rho = seq(from = 0.4, to = 0.9, by = 0.1)
+
+    range_exposure_sigma  = seq(from = 0.5, to =3, by = 0.2)
+    range_exposure_baseline = seq(from = 10, to = 40, by = 10)
+
+    picked_sr_sigma = sample(range_sr_sigma, 2)
+    picked_sr_rho = sample(range_sr_rho, 1)
+
+    picked_dr_sigma = sample(range_dr_sigma, 1)
+    picked_dr_rho = sample(range_dr_rho, 1)
+
+    picked_exposure_sigma  = sample(range_exposure_sigma, 1)
+    picked_exposure_baseline  = sample(range_exposure_baseline, 1)
+
+
     # Make data--------------------------------
     N_id = grid_subsample$N_id[i]
 
@@ -80,7 +98,6 @@ simulations <- function(
       B[1,3] = -8.9
       B[3,2] = -7.9
     }
-
 
     block = data.frame(Clique=factor(clique))
 
@@ -95,14 +112,15 @@ simulations <- function(
       groups=groups,
       individual_predictors=Hairy,
       individual_effects=matrix(c(grid_subsample$tie_effect[i], grid_subsample$tie_effect[i]),ncol=1, nrow=2),
-      sr_sigma = sr_sigma,
-      sr_rho = sr_rho,
-      dr_sigma = dr_sigma,
-      dr_rho = dr_rho,
+
+      sr_sigma = picked_sr_sigma,
+      sr_rho = picked_sr_rho,
+      dr_sigma = picked_dr_sigma,
+      dr_rho = picked_dr_rho,
       exposure_predictors = cbind(rep(1,N_id),Hairy),
       exposure_effects = c(-1, grid_subsample$hairy_detect_effect[i]),
-      exposure_sigma = exposure_sigma,
-      exposure_baseline =exposure_baseline,
+      exposure_sigma = picked_exposure_sigma,
+      exposure_baseline = picked_exposure_baseline,
       int_bias = T
     )
 
@@ -121,7 +139,7 @@ simulations <- function(
       )
 
       # !!  I replaced fit_block_plus_social_relations_model by fit_social_relations_model
-      if(blocks){
+      if(run_blocks_model){
         fit =  fit_block_plus_social_relations_model(data=model_dat,
                                                      block_regression = ~ Clique,
                                                      focal_regression = ~ Hairy,
@@ -134,14 +152,14 @@ simulations <- function(
         )
       }else{
         fit =  fit_social_relations_model(data=model_dat,
-                                            focal_regression = ~ Hairy,
-                                            target_regression = ~ Hairy,
-                                            dyad_regression = ~  1,
-                                            mode="mcmc",
-                                            stan_mcmc_parameters = list(chains = 1, parallel_chains = 1, refresh = 1,
-                                                                        iter_warmup = 1000, iter_sampling = 1000,
-                                                                        max_treedepth = NULL, adapt_delta = .98)
-                                        )
+                                          focal_regression = ~ Hairy,
+                                          target_regression = ~ Hairy,
+                                          dyad_regression = ~  1,
+                                          mode="mcmc",
+                                          stan_mcmc_parameters = list(chains = 1, parallel_chains = 1, refresh = 1,
+                                                                      iter_warmup = 1000, iter_sampling = 1000,
+                                                                      max_treedepth = NULL, adapt_delta = .98)
+        )
 
       }
 
@@ -157,8 +175,14 @@ simulations <- function(
       rownames(result) = NULL
       result$approach = 'strand'
       result$sim = i
+      result$sr_sigma = paste(picked_sr_sigma[1], picked_sr_sigma[2], sep = "/")
+      result$sr_rho  = picked_sr_rho
+      result$dr_sigma = picked_dr_sigma
+      result$dr_rho = picked_dr_rho
+      result$exposure_sigma  = picked_exposure_sigma
+      result$exposure_baseline = picked_exposure_baseline
 
-      colnames(result) = c('scale(hair)','5 %','95 %', 'N_id', 'tie_effect', 'detect_effect', 'p-value', 'approach', 'sim')
+      colnames(result) = c('scale(hair)','5 %','95 %', 'N_id', 'tie_effect', 'detect_effect', 'p-value', 'approach', 'sim', 'sr_sigma', 'sr_rho', 'dr_sigma','dr_rho','exposure_sigma','exposure_baseline')
       RESULTS = rbind(RESULTS, result)
     }
 
@@ -166,8 +190,7 @@ simulations <- function(
     tie_strength = A$network/(1+A$true_samps)
     colnames(tie_strength) = rownames(tie_strength) = 1:ncol(tie_strength)
     df = ANTs:::df.create(tie_strength)
-    df = met.strength(tie_strength, df = df, dfid = 1)
-    df = met.degree(tie_strength, df = df, dfid = 1)
+    df$strength = met.strength(tie_strength)
     df$hair = indiv$Hairy
     test1.1 = lm(strength ~ hair, data = df)
 
@@ -176,7 +199,15 @@ simulations <- function(
     rownames(result) = NULL
     result$approach = 'Rates unweighted'
     result$sim = i
-    colnames(result) = c('scale(hair)','5 %','95 %', 'N_id', 'tie_effect', 'detect_effect', 'p-value', 'approach', 'sim')
+    result$sr_sigma = paste(picked_sr_sigma[1], picked_sr_sigma[2], sep = "/")
+    result$sr_rho  = picked_sr_rho
+    result$dr_sigma = picked_dr_sigma
+    result$dr_rho = picked_dr_rho
+    result$exposure_sigma  = picked_exposure_sigma
+    result$exposure_baseline = picked_exposure_baseline
+
+    colnames(result) = c('scale(hair)','5 %','95 %', 'N_id', 'tie_effect', 'detect_effect', 'p-value',
+                         'approach', 'sim', 'sr_sigma', 'sr_rho', 'dr_sigma','dr_rho','exposure_sigma','exposure_baseline')
     RESULTS = rbind(RESULTS, result)
 
 
@@ -186,8 +217,14 @@ simulations <- function(
     rownames(result) = NULL
     result$approach = 'Rates weighted'
     result$sim = i
-    result$sim = i
-    colnames(result) = c('scale(hair)','5 %','95 %', 'N_id', 'tie_effect', 'detect_effect', 'p-value', 'approach', 'sim')
+    result$sr_sigma = paste(picked_sr_sigma[1], picked_sr_sigma[2], sep = "/")
+    result$sr_rho  = picked_sr_rho
+    result$dr_sigma = picked_dr_sigma
+    result$dr_rho = picked_dr_rho
+    result$exposure_sigma  = picked_exposure_sigma
+    result$exposure_baseline = picked_exposure_baseline
+
+    colnames(result) = c('scale(hair)','5 %','95 %', 'N_id', 'tie_effect', 'detect_effect', 'p-value', 'approach', 'sim', 'sr_sigma', 'sr_rho', 'dr_sigma','dr_rho','exposure_sigma','exposure_baseline')
     RESULTS = rbind(RESULTS, result)
 
     #  SRI of interactions unweighted--------------------------------
@@ -210,8 +247,7 @@ simulations <- function(
     sri[is.na(sri)] = 0
     colnames(sri)= rownames(sri) = 1:ncol(sri)
     df = ANTs:::df.create(sri)
-    df = met.strength(sri, df = df, dfid = 1)
-    df = met.degree(sri, df = df, dfid = 1)
+    df$strength = met.strength(sri)
     df$hair = indiv$Hairy
 
     test1.1 = lm(strength ~ hair, data = df, weights = A$true_exposure)
@@ -220,7 +256,14 @@ simulations <- function(
     rownames(result) = NULL
     result$approach = 'SRI unweigthed'
     result$sim = i
-    colnames(result) = c('scale(hair)','5 %','95 %', 'N_id', 'tie_effect', 'detect_effect', 'p-value', 'approach', 'sim')
+    result$sr_sigma = paste(picked_sr_sigma[1], picked_sr_sigma[2], sep = "/")
+    result$sr_rho  = picked_sr_rho
+    result$dr_sigma = picked_dr_sigma
+    result$dr_rho = picked_dr_rho
+    result$exposure_sigma  = picked_exposure_sigma
+    result$exposure_baseline = picked_exposure_baseline
+
+    colnames(result) = c('scale(hair)','5 %','95 %', 'N_id', 'tie_effect', 'detect_effect', 'p-value', 'approach', 'sim', 'sr_sigma', 'sr_rho', 'dr_sigma','dr_rho','exposure_sigma','exposure_baseline')
     RESULTS = rbind(RESULTS, result)
 
     #  SRI of interactions weighted--------------------------------
@@ -230,49 +273,83 @@ simulations <- function(
     rownames(result) = NULL
     result$approach = 'SRI weighted'
     result$sim = i
-    colnames(result) = c('scale(hair)','5 %','95 %', 'N_id', 'tie_effect', 'detect_effect', 'p-value', 'approach', 'sim')
+    result$sr_sigma = paste(picked_sr_sigma[1], picked_sr_sigma[2], sep = "/")
+    result$sr_rho  = picked_sr_rho
+    result$dr_sigma = picked_dr_sigma
+    result$dr_rho = picked_dr_rho
+    result$exposure_sigma  = picked_exposure_sigma
+    result$exposure_baseline = picked_exposure_baseline
+
+    colnames(result) = c('scale(hair)','5 %','95 %', 'N_id', 'tie_effect', 'detect_effect', 'p-value', 'approach', 'sim', 'sr_sigma', 'sr_rho', 'dr_sigma','dr_rho','exposure_sigma','exposure_baseline')
     RESULTS = rbind(RESULTS, result)
   }
 
+  #############################
+  #######  Return #############
+  #############################
   parallel::stopCluster(cl)
   gc()
   registerDoSEQ()
-  return(do.call('rbind', r))
+  result = do.call('rbind', r)
+  result$z = result$`scale(hair)`/(result$`95 %` - result$`5 %`)
+  result$z = ifelse(result$approach !=  "strand", result$`scale(hair)`/(result$`95 %` - result$`5 %`), result$`scale(hair)`)
+  result$resid = result$tie_effect - result$z
+
+  result$approach = ifelse(result$approach == "strand", '1.Bayesian', result$approach)
+  result$approach = ifelse(result$approach == "Rates unweighted", '2.Rates', result$approach)
+  result$approach = ifelse(result$approach == "SRI unweigthed", '3.SRI', result$approach)
+
+  return(result)
 }
 
-result = simulations(Reps = 50, strand = F, ncores = 4, blocks = TRUE)
-result$resid = result$tie_effect - result$`scale(hair)`
-result$z = result$`scale(hair)`/(result$`95 %` - result$`5 %`)
 
 # Plot ----------
+plots <- function(result){
+  require(ggplot2)
+  p1 = ggplot(result, aes(x= z,  y= tie_effect, color = sim, group = sim, label = z))+
+    #geom_linerange(aes(xmin=result[,2], xmax=result[,3])) +
+    geom_point(aes(color = sim, size = 1), show.legend = FALSE, alpha = 0.5) +
+    facet_grid( . ~ approach, space="free") +
+    theme(legend.position = 'none')+
+    xlim(min(result$z), max(result$z)) +
+    ylim(min(result$z), max(result$z)) +
+    geom_abline()+
+    ylab("True efect size") +
+    xlab("Estimated effect size") +
+    theme(axis.text = element_text(size = 14))  +
+    theme(axis.title = element_text(size = 14))
+
+
+  p2 = ggplot(result, aes(x = resid, y = tie_effect, color = approach))+
+    geom_point(aes(size = N_id),  alpha = 0.5, show.legend = FALSE)+
+    geom_vline(xintercept = 0, linetype = "dashed")+
+    xlab("Difference between true effect size and estimated effect size")+
+    ylab("True effect size")+
+    theme(axis.text = element_text(size = 14))+
+    theme(axis.title = element_text(size = 14))+
+    theme(legend.position = 'none')
+
+  p3 = ggplot(result, aes(x= approach, y = resid, color = approach, group = approach))+
+    geom_violin()+
+    stat_summary(fun= function(i){r = quantile(i)[2:4]},  shape=23, size=10, geom = 'point', color = 'black')+
+    geom_jitter(aes(size = 1), alpha = 0.5, show.legend = FALSE, position=position_jitter(0.2)) +
+    theme(axis.text = element_text(size = 14))+
+    theme(axis.title = element_text(size = 14))+
+    theme(legend.position = 'none')
+  return(list(p1, p2, p3))
+}
+
+
 library(ggpubr)
-require(ggplot2)
-p1 = ggplot(result, aes(x=z,  y= tie_effect , xmin=result[,2], xmax=result[,3],  group = sim, label = z))+
-  #geom_errorbar() +
-  geom_point(aes(color = N_id, size = detect_effect), alpha = 0.5) +
-  facet_grid( . ~ approach, space="free") +
-  xlim(c(-5,5))+
-  ylim(c(-5,5))+
-  geom_abline()+
-  ylab("True efect size") +
-  xlab("Estimated effect size") +
-  theme(axis.text = element_text(size = 14))  +
-  theme(axis.title = element_text(size = 14))
-p1
+result = simulations(Reps = 1, strand = F, ncores = 1)
+tmp = result[result$approach %in% c('1.Bayesian', '2.Rates', '3.SRI'),]
+p = plots(tmp)
+ggarrange(p[[1]], p[[3]], ncol = 1, nrow = 2)
 
-p2 = ggplot(result, aes(x = resid, y = detect_effect, color = approach))+
-  geom_point(aes(size = detect_effect), show.legend = FALSE, alpha = 0.5)+
-  geom_vline(xintercept = 0, linetype = "dashed")+
-  xlab("Difference between true effect size and estimated effect size")+
-  ylab("True effect size")+
-  theme(axis.text = element_text(size = 14))+
-  theme(axis.title = element_text(size = 14))
+# Analysis ----------
+library(lmerTest)
+library(emmeans)
+m = lmer(formula = resid ~ approach + (1|tie_effect), data = tmp)
+summary(m)
 
-
-p3 = ggplot(result, aes(x = tie_effect,  y = `p-value`,  group = approach, color = approach))+
-  geom_point()+
-  geom_hline(yintercept=0.05, linetype="dashed",
-             color = "red", size=1)
-p3
-ggarrange(p1, p2, ncol  = 1, nrow = 2, common.legend = FALSE)
-
+emmeans(m, list(pairwise ~ approach), adjust = "tukey")
