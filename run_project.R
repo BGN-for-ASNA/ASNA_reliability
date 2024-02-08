8f45842cd75628a80c67f1ff92dd87f50c77e6c6
Sebastian Sosa <sebastian_sosa@EVA.MPG.DE>
2024-02-08 09:14:17 +0100

    Install dependencies before runing project

--- run_project.R
+++ run_project.R
@@ -1,3 +1,7 @@
+list.of.packages <- c("rmarkdown", "ggpubr", 'ggplot2','ggrepel', 'ANTs', 'STRAND', 'lmerTest', 'bisonR', 'rstan')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(rmarkdown)
 library(ggpubr)
 library(ggplot2)
